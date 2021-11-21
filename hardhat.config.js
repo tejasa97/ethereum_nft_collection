24e787f93d7e1636cf15d42d6d0451687a6e0e81
Tejas Arlimatti <tejasarlimatti@gmail.com>
2021-11-21 19:35:56 +0530

    Added script and changes for Testnet deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +19,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: process.env.WEB3_INFURA_URL,
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ],
+    },
+  },
 };
